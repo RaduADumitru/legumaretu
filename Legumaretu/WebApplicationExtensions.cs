31a5998714cb589a13c4dbcd5e019ad5dba1ce82
RaduADumitru <radu.dumitru@rocketmail.com>
2023-12-29 19:52:10 +0200

    Create admin user on startup

--- Legumaretu/WebApplicationExtensions.cs
+++ Legumaretu/WebApplicationExtensions.cs
@@ -8,6 +8,7 @@ public static class WebApplicationExtensions
 	{
 		using var scope = app.Services.CreateScope();
 		var roleManager = (RoleManager<IdentityRole>)scope.ServiceProvider.GetService(typeof(RoleManager<IdentityRole>));
+		var userManager = (UserManager<IdentityUser>)scope.ServiceProvider.GetService(typeof(UserManager<IdentityUser>));
 		var roles = configuration.GetSection("Roles").Get<List<string>>();
 
 		foreach (var role in roles)
@@ -15,7 +16,28 @@ public static class WebApplicationExtensions
 			if (!await roleManager.RoleExistsAsync(role))
 				await roleManager.CreateAsync(new IdentityRole(role));
 		}
+		// find the user with the admin email 
+		var _user = await userManager.FindByEmailAsync("admin@email.com");
 
+		// check if the user exists
+		if (_user == null)
+		{
+			//Here you could create the super admin who will maintain the web app
+			var poweruser = new IdentityUser
+			{
+				UserName = "admin@email.com",
+				Email = "admin@email.com",
+			};
+			string adminPassword = "P@ssw0rd";
+
+			var createPowerUser = await userManager.CreateAsync(poweruser, adminPassword);
+			if (createPowerUser.Succeeded)
+			{
+				//here we tie the new user to the role
+				await userManager.AddToRoleAsync(poweruser, "Admin");
+
+			}
+		}
 		return app;
 	}
 }
