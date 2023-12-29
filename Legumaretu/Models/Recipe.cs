3c6782a5a59440e2cd233de1a324e007fe7149d8
nubaz <lucapaulf@gmail.com>
2023-12-29 23:09:47 +0200

    Anonymous user landing page (WIP)

---
+++ Legumaretu/Models/Recipe.cs
@@ -0,0 +1,22 @@
+﻿namespace Legumaretu.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Stars { get; set; }
+        public bool Official { get; set; }
+        public string ImgLink { get; set; }
+
+        public Recipe(int id, string name, string description, int stars, bool official, string imglink)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Stars = stars;
+            Official = official;
+            ImgLink = imglink;
+        }
+    }
+}
