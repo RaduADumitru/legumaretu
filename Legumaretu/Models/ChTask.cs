b3100927899c337d4777f86a53c6addb7ee4004d
nubaz <lucapaulf@gmail.com>
2023-12-30 18:03:55 +0200

    Added challenge, dummy data service classes; Code cleanup

---
+++ Legumaretu/Models/ChTask.cs
@@ -0,0 +1,16 @@
+﻿namespace Legumaretu.Models
+{
+	public class ChTask
+	{
+		public int Id { get; set; }
+		public Recipe Recipe { get; set; }
+		public bool Done { get; set; }
+
+		public ChTask(int id, Recipe recipe)
+		{
+			Id = id;
+			Recipe = recipe;
+			Done = false;
+		}
+	}
+}
