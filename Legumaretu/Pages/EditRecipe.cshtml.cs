41b5dcaa41e6f359c6f2ff145a7962f5aa525e52
RaduADumitru <radu.dumitru@rocketmail.com>
2024-01-02 20:05:00 +0200

    Edit recipe auth

---
+++ Legumaretu/Pages/EditRecipe.cshtml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Legumaretu.Data;
+using Legumaretu.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace Legumaretu.Pages
+{
+    [Authorize(Roles = "Default,Moderator,Admin")]
+    public class EditRecipeModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly Legumaretu.Data.ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public EditRecipeModel(ILogger<IndexModel> logger, Legumaretu.Data.ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _logger = logger;
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [BindProperty]
+        public Recipe Recipe { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Recipes == null)
+            {
+                return NotFound();
+            }
+
+            var recipe =  await _context.Recipes.FirstOrDefaultAsync(m => m.Id == id);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+            Recipe = recipe;
+            // Default users can only edit their own recipes
+            if (!User.IsInRole("Admin") && !User.IsInRole("Moderator"))
+            {
+                ApplicationUser user = _userManager.GetUserAsync(User).Result;
+                if (user == null || recipe.User.Id != user.Id)
+                {
+                    return NotFound();
+                }
+            }
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Recipe).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RecipeExists(Recipe.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool RecipeExists(int id)
+        {
+          return (_context.Recipes?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
