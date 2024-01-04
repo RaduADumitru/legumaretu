c86bf8ba7545899e9faccd6c5b6de65dad1d9b99
RaduADumitru <radu.dumitru@rocketmail.com>
2024-01-04 20:12:54 +0200

    Edit Challenge

--- Legumaretu/Pages/EditChallenge.cshtml.cs
+++ Legumaretu/Pages/EditChallenge.cshtml.cs
@@ -8,15 +8,22 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Legumaretu.Data;
 using Legumaretu.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Legumaretu.Pages
 {
+    [Authorize(Roles = "Default,Moderator,Admin")]
     public class EditChallengeModel : PageModel
     {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
         private readonly Legumaretu.Data.ApplicationDbContext _context;
 
-        public EditChallengeModel(Legumaretu.Data.ApplicationDbContext context)
+        public EditChallengeModel(ILogger<IndexModel> logger, UserManager<ApplicationUser> userManager, ApplicationDbContext context)
         {
+            _logger = logger;
+            _userManager = userManager;
             _context = context;
         }
 
@@ -36,6 +43,15 @@ namespace Legumaretu.Pages
                 return NotFound();
             }
             Challenge = challenge;
+            // Default users can only edit their own challenges
+            if (!User.IsInRole("Admin") && !User.IsInRole("Moderator"))
+            {
+                ApplicationUser user = _userManager.GetUserAsync(User).Result;
+                if (user == null || challenge.User.Id != user.Id)
+                {
+                    return NotFound();
+                }
+            }
             return Page();
         }
 
