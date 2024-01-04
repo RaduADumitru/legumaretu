f5aa1953d6cef623b6af5e86bfd13daae895848c
RaduADumitru <radu.dumitru@rocketmail.com>
2024-01-04 20:05:30 +0200

    Create challenge

--- Legumaretu/Models/Challenge.cs
+++ Legumaretu/Models/Challenge.cs
@@ -1,13 +1,17 @@
-﻿namespace Legumaretu.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Legumaretu.Models
 {
 	public class Challenge
 	{
 		public int Id {  get; set; }
-		public string Name { get; set; }
+		[Required]
+        [StringLength(50)]
+        public string Name { get; set; }
 		public string? Description { get; set; }
 		public bool Official {  get; set; }
-		public List<Recipe> Recipes { get; set; }
-		public ApplicationUser User { get; set; }
+		public List<Recipe>? Recipes { get; set; }
+		public ApplicationUser? User { get; set; }
 
 		public Challenge(int id, string name, string description, bool official, List<Recipe> recipes)
 		{
---
+++ Legumaretu/Pages/EditChallenge.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Legumaretu.Data;
+using Legumaretu.Models;
+
+namespace Legumaretu.Pages
+{
+    public class EditChallengeModel : PageModel
+    {
+        private readonly Legumaretu.Data.ApplicationDbContext _context;
+
+        public EditChallengeModel(Legumaretu.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Challenge Challenge { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Challenges == null)
+            {
+                return NotFound();
+            }
+
+            var challenge =  await _context.Challenges.FirstOrDefaultAsync(m => m.Id == id);
+            if (challenge == null)
+            {
+                return NotFound();
+            }
+            Challenge = challenge;
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Challenge).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ChallengeExists(Challenge.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool ChallengeExists(int id)
+        {
+          return (_context.Challenges?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
