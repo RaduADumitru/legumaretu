579276382c199005048cd4ed3b95ec49c0959a16
nubaz <lucapaulf@gmail.com>
2024-01-10 15:36:27 +0200

    SingUpForChallenge, Main title

--- Legumaretu/Models/ChallengeProgress.cs
+++ Legumaretu/Models/ChallengeProgress.cs
@@ -7,4 +7,14 @@ public class ChallengeProgress
 	public int ChallengeId { get; set; }
 	public Challenge? Challenge { get; set; }
 	public virtual List<ChTask>? ChTasks { get; set; }
+
+	public ChallengeProgress(int id, int challengeId, Challenge challenge, List<ChTask> chTasks)
+	{
+		Id = id; 
+		ChallengeId = challengeId;
+		Challenge = challenge; 
+		ChTasks = chTasks;
+	}
+
+	public ChallengeProgress() {  }
 }
