f3221bde1123405f207855aa7ae08d949856dc92
RaduADumitru <radu.dumitru@rocketmail.com>
2024-01-15 20:15:35 +0200

    Filter recipes according to user role

--- Legumaretu/Pages/Index.cshtml.cs
+++ Legumaretu/Pages/Index.cshtml.cs
@@ -1,36 +1,59 @@
 ﻿using Legumaretu.Data;
 using Legumaretu.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Legumaretu.Pages
 {
-    public class IndexModel : PageModel
-    {
-        private readonly ILogger<IndexModel> _logger;
-        private readonly ApplicationDbContext _context;
-        public List<Recipe> Recipes { get; set; }
+	public class IndexModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+		private readonly ApplicationDbContext _context;
+		private readonly SignInManager<ApplicationUser> _signInManager;
+		private readonly UserManager<ApplicationUser> _userManager;
+		public List<Recipe> Recipes { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext context)
-        {
-            _logger = logger;
-            _context = context;
-        }
+		public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext context, SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+		{
+			_logger = logger;
+			_context = context;
+			_signInManager = signInManager;
+			_userManager = userManager;
+		}
 
-        public void OnGet(string searchStr, string starFilter)
+		public void OnGet(string searchStr, string starFilter)
 		{
 			if (!String.IsNullOrEmpty(searchStr))
 			{
 				searchStr = searchStr.Trim().ToLower();
-				Recipes = _context.Recipes.Where(r => r.Name.Trim().ToLower().Contains(searchStr)).ToList();
+				Recipes = _context.Recipes.Include(r => r.User).Where(r => r.Name.Trim().ToLower().Contains(searchStr)).ToList();
 			}
-			else if (!String.IsNullOrEmpty(starFilter)) 
+			else if (!String.IsNullOrEmpty(starFilter))
 			{
-                Recipes = _context.Recipes.Where(r => r.Stars == int.Parse(starFilter)).ToList();
-            }
+				Recipes = _context.Recipes.Include(r => r.User).Where(r => r.Stars == int.Parse(starFilter)).ToList();
+			}
 			else
 			{
-				Recipes = _context.Recipes.ToList();
+				Recipes = _context.Recipes.Include(r => r.User).ToList();
 			}
-        }
+			//if not logged in, show only official recipes
+			if (!_signInManager.IsSignedIn(User))
+			{
+				Recipes = Recipes.Where(r => r.Official).ToList();
+			}
+			else if (User.IsInRole("Moderator") || User.IsInRole("Administrator"))
+			{
+				//show all recipes
+				Recipes = Recipes.ToList();
+			}
+			else
+			{
+				//show only official recipes and own recipes
+				String userId = _userManager.GetUserId(User);
+				Recipes = Recipes.Where(r => r.Official || r.User.Id == userId).ToList();
+			}
+
+		}
 	}
 }
