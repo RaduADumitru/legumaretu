26f9033d07c19252f430ef6c2deacbb3bf1c2da0
RaduADumitru <radu.dumitru@rocketmail.com>
2024-01-15 20:27:20 +0200

    WIP challenge filter

--- Legumaretu/Pages/Challenges.cshtml.cs
+++ Legumaretu/Pages/Challenges.cshtml.cs
@@ -1,5 +1,6 @@
 using Legumaretu.Data;
 using Legumaretu.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,14 +10,18 @@ namespace Legumaretu.Pages
     {
         private readonly ILogger<IndexModel> _logger;
         private readonly ApplicationDbContext _context;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
         public List<Challenge> Challenges { get; set; }
         public int MinPoints { get; set; }
         public int MaxPoints { get; set; }
 
-        public ChallengesModel(ILogger<IndexModel> logger, ApplicationDbContext context)
+        public ChallengesModel(ILogger<IndexModel> logger, ApplicationDbContext context, SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
         {
             _logger = logger;
             _context = context;
+            _signInManager = signInManager;
+            _userManager = userManager;
             
             MinPoints = _context.Challenges.Include(c => c.Recipes).ToList().Min(c => c.TotalPoints());
             MaxPoints = _context.Challenges.Include(c => c.Recipes).ToList().Max(c => c.TotalPoints());
@@ -27,17 +32,33 @@ namespace Legumaretu.Pages
             if (!String.IsNullOrEmpty(searchStr))
             {
                 searchStr = searchStr.Trim().ToLower();
-                Challenges = _context.Challenges.Include(c => c.Recipes).Where(c => c.Name.Trim().ToLower().Contains(searchStr)).ToList();
+                Challenges = _context.Challenges.Include(c => c.User).Include(c => c.Recipes).Where(c => c.Name.Trim().ToLower().Contains(searchStr)).ToList();
             }
             else if (!String.IsNullOrEmpty(filterValue))
             {
                 int value = int.Parse(filterValue);
-                Challenges = _context.Challenges.Include(c => c.Recipes).AsEnumerable().Where(c => c.TotalPoints() <= value).ToList();
+                Challenges = _context.Challenges.Include(c => c.User).Include(c => c.Recipes).AsEnumerable().Where(c => c.TotalPoints() <= value).ToList();
             }
             else
             {
-                Challenges = _context.Challenges.Include(c => c.Recipes).ToList();
+                Challenges = _context.Challenges.Include(c => c.User).Include(c => c.Recipes).ToList();
             }
-        }
+            //if not logged in, show only official challenges
+            if (!_signInManager.IsSignedIn(User))
+            {
+	            Challenges = Challenges.Where(c => c.Official).ToList();
+            }
+            else if (User.IsInRole("Moderator") || User.IsInRole("Administrator"))
+            {
+	            //show all recipes
+	            Challenges = Challenges.ToList();
+            }
+            else
+            {
+	            //show only official recipes and own recipes
+	            String userId = _userManager.GetUserId(User);
+	            Challenges = Challenges.Where(c => c.Official || c.User.Id == userId).ToList();
+            }
+		}
     }
 }
